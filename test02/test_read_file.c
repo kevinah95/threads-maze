dd005e7222737e0994e15037b8eb18d9a69cf666
Kevin Hernandez Rostran <kevinah95@gmail.com>
2017-03-30 00:42:01 -0600

    New File Reader Test01

---
+++ test02/test_read_file.c
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    FILE *f = fopen("examples/maze.txt", "rb");
+    fseek(f, 0, SEEK_END);
+    long pos = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char *bytes = malloc(pos);
+    //fread(bytes, pos, 1, f);
+    //fgets(bytes,pos,f);
+    //fscanf(f,"%c*",bytes);
+    fscanf(f, "%*c %[^\\n\\r]", bytes);
+
+    fclose(f);
+    //bytes[strcspn(bytes, "\n")] = ' ';
+    char* temp = strstr(bytes, "\n");
+    // Remove \n.
+    while ((temp = strstr(bytes, "\n")) != NULL) {
+    // Len is the length of the string, from the ampersand \n, including the \n.
+        int len = strlen(bytes);
+        memmove(temp, temp + 1, len);
+    }
+    printf("String: ");
+    printf("%s",bytes);
+    printf("Len: ");
+    printf("%d",strlen(bytes));
+
+
+    return 0;
+    /*free(temp); // free allocated memory
+    free(bytes); // free allocated memory*/
+}
